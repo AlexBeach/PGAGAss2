2004ca2c0ff41b98bc9e2b6a7eb1659e3a2cb9b7
AlexBeach <i7257741@PC40573.student.bournemouth.ac.uk>
2015-04-16 19:06:21 +0100

    Fixes

---
+++ Project/PGG_Lab14/Shader.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <glm.hpp>
+#include <SDL.h>
+#include "glew.h"
+
+class Shader
+{
+public:
+	Shader(void);
+	~Shader(void);
+
+	/// Loads shaders for the object into OpenGL
+	void InitialiseShaders();
+
+	inline	GLuint Program() { return program; }
+	inline GLint ModelMat() { return ModelMatLocation; }
+	inline GLint ViewMat() { return ViewMatLocation; }
+	inline GLint ProjMat() { return ProjMatLocation; }
+
+private:
+
+	/// Shader program
+	GLuint program;
+
+	/// Uniform locations
+	GLint ModelMatLocation, ViewMatLocation, ProjMatLocation;
+};
+
